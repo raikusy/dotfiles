6374f5784b67928481bad4095a7d5229f57faa6f
Rakibul Hasan <xenax.rakibul@gmail.com>
2024-11-13 05:11:40 +0600

    fix: properly pass self to configuration

--- configuration.nix
+++ configuration.nix
@@ -1,62 +1,29 @@
-{ config, pkgs, ... }:
+{ pkgs, self, ... }:
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = [
-    # pkgs.zsh
-    # pkgs.fish
-    # pkgs.vim
-    # pkgs.git
-    # pkgs.curl
-    # pkgs.wget
-    # pkgs.ripgrep
-    # pkgs.fzf
-    # pkgs.lsd
-    # pkgs.yazi
-    # pkgs.bat
-    # pkgs.zoxide
-    # pkgs.starship
-    # pkgs.nixfmt-rfc-style
-    # pkgs.rustc
-    # pkgs.cargo
-    # pkgs.rustfmt
-    # pkgs.direnv
-    # pkgs.colima
-    # pkgs.devenv
-    # pkgs.nixd
-    # pkgs.nil
+  environment.systemPackages = with pkgs; [
+    # Add some essential packages
+    coreutils
+    curl
+    git
   ];
 
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
-  # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  # services.nix-daemon.package = pkgs.nixFlakes;
-  services.activate-system.enable = true;
-  nix.package = pkgs.nix;
 
   # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
-
-  # Programs
-  programs = {
-    direnv.enable = true;
-    zsh = {
-      enable = true;
-      enableFastSyntaxHighlighting = true;
-      enableFzfCompletion = true;
-    };
-    fish = {
-      enable = true;
-      loginShellInit = ''
-        set --universal ME (whoami)
-        fish_add_path /etc/profiles/per-user/$ME/bin/
-        eval (${pkgs.starship}/bin/starship init fish)
-      '';
-    };
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    trusted-users = [
+      "root"
+      "raikusy"
+      "@wheel"
+    ];
+    use-xdg-base-directories = true;
+    accept-flake-config = true;
   };
 
   # Set Git commit hash for darwin-version.
-  system.configurationRevision = config.rev or config.dirtyRev or null;
+  system.configurationRevision = self.rev or self.dirtyRev or null;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
@@ -65,6 +32,10 @@
   # The platform the configuration will be used on.
   nixpkgs = {
     hostPlatform = "aarch64-darwin";
+    config = {
+      allowBroken = true;
+      allowUnfree = true;
+    };
   };
 
   security.pam.enableSudoTouchIdAuth = true;
--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Darwin configuration";
 
   inputs = {
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -10,10 +11,12 @@
   };
 
   outputs =
-    inputs@{
+    {
       self,
+      determinate,
       home-manager,
       darwin,
+      nixpkgs,
       ...
     }:
     {
@@ -21,24 +24,26 @@
         raikusy = darwin.lib.darwinSystem {
           system = "aarch64-darwin";
           modules = [
+            # Pass self as a module argument
+            {
+              _module.args = {
+                inherit self;
+              };
+            }
             ./configuration.nix
             home-manager.darwinModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.raikusy = import ./home.nix;
-              # home-manager.extraSpecialArgs = {
-              #   inherit inputs;
-              # };
-              # Optionally, use home-manager.extraSpecialArgs to pass
-              # arguments to home.nix
+              home-manager.backupFileExtension = "backup";
             }
+            determinate.darwinModules.default
           ];
         };
       };
 
       # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations."raikusy".pkgs;
-
     };
 }
