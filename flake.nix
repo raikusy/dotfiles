c737dd2a477741e3a95388af64cb3758bd7d368f
raikusy <xenax.rakibul@gmail.com>
2025-06-24 05:01:15 +0600

    Refactor Nix configuration: rename darwin input to nix-darwin for consistency and clarity in flake.nix.

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     # determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    darwin.url = "github:nix-darwin/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
