e66b43bfee5b9d9a7eac1f221c93e8b52190b143
raikusy <xenax.rakibul@gmail.com>
2025-09-27 23:51:01 +0600

    Update configuration files: add KiroAgent setting to VSCode, initialize Conda in Fish shell configuration, and change font size in Ghostty configuration for improved readability and aesthetics.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -99,3 +99,16 @@ abbr -a -- :sf superfile
 abbr -a -- :cr cursor
 abbr -a -- :wt wezterm
 abbr -a -- :ws windsurf
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
